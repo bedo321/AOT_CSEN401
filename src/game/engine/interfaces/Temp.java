5b83a8fe1f8dacd0f763af04e77a1b3f88930896
bedo321 <abdalrhman.ashry@gmail.com>
2024-02-22 20:47:04 +0200

    Added Interfaces Package

--- src/game/engine/interfaces/Temp.java
+++ src/game/engine/interfaces/Temp.java
@@ -2,6 +2,6 @@ package game.engine.interfaces;
 
 public class Temp {
     public static void main(String[] args) {
-        System.out.println("Don't Remove this unless there is another class in the package");
+        System.out.println("Don't Remove this unless there is another class the package");
     }
 }
