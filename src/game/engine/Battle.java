fed3707c7b4be0be0f3748216226483e247f7137
bedo321 <abdalrhman.ashry@gmail.com>
2024-02-24 00:52:10 +0200

    Implemented the `Battle` class

--- src/game/engine/Battle.java
+++ src/game/engine/Battle.java
@@ -1,4 +1,117 @@
 package game.engine;
 
+import game.engine.base.Wall;
+import game.engine.lanes.Lane;
+import game.engine.titans.Titan;
+import game.engine.weapons.factory.WeaponFactory;
+import game.engine.titans.TitanRegistry;
+import game.engine.dataloader.DataLoader;
+import java.util.*;
+import java.io.*;
+
 public class Battle {
+    private final static int[][] PHASES_APPROACHING_TITANS = new int[3][];
+    private final static int WALL_BASE_HEALTH = 10000;
+    private int numberOfTurns;
+    private int resourcesGathered;
+    private BattlePhase battlePhase;
+    private int numberOfTitansPerTurn;
+    private int score;
+    private int titanSpawnDistance;
+    private final WeaponFactory weaponFactory;
+    private final HashMap<Integer, TitanRegistry> titansArchive;
+    private final ArrayList<Titan> approachingTitans;
+    private final PriorityQueue<Lane> lanes;
+    private final ArrayList<Lane> originalLanes;
+
+    Battle(int numberOfTurns, int score, int titanSpawnDistance, int initialNumOfLanes,
+           int initialResourcesPerLane) throws IOException {
+        this.numberOfTurns = numberOfTurns;
+        this.resourcesGathered = initialResourcesPerLane * initialNumOfLanes;
+        this.battlePhase = BattlePhase.EARLY;
+        this.numberOfTitansPerTurn = 1;
+        this.score = score;
+        this.titanSpawnDistance = titanSpawnDistance;
+        this.weaponFactory = new WeaponFactory();
+        this.titansArchive = DataLoader.readTitanRegistry();
+        this.approachingTitans = new ArrayList<>();
+        this.lanes = new PriorityQueue<>();
+        this.originalLanes = new ArrayList<>();
+    }
+
+    private void initializeLanes(int numOfLanes) {
+        for(int i = 0; i < numOfLanes; i++) {
+            Lane lane = new Lane(new Wall(WALL_BASE_HEALTH));
+            lanes.add(lane);
+            originalLanes.add(lane);
+        }
+    }
+
+    public int getNumberOfTurns() {
+        return numberOfTurns;
+    }
+
+    public void setNumberOfTurns(int numberOfTurns) {
+        this.numberOfTurns = numberOfTurns;
+    }
+
+    public int getResourcesGathered() {
+        return resourcesGathered;
+    }
+
+    public void setResourcesGathered(int resourcesGathered) {
+        this.resourcesGathered = resourcesGathered;
+    }
+
+    public BattlePhase getBattlePhase() {
+        return battlePhase;
+    }
+
+    public void setBattlePhase(BattlePhase battlePhase) {
+        this.battlePhase = battlePhase;
+    }
+
+    public int getNumberOfTitansPerTurn() {
+        return numberOfTitansPerTurn;
+    }
+
+    public void setNumberOfTitansPerTurn(int numberOfTitansPerTurn) {
+        this.numberOfTitansPerTurn = numberOfTitansPerTurn;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getTitanSpawnDistance() {
+        return titanSpawnDistance;
+    }
+
+    public void setTitanSpawnDistance(int titanSpawnDistance) {
+        this.titanSpawnDistance = titanSpawnDistance;
+    }
+
+    public WeaponFactory getWeaponFactory() {
+        return weaponFactory;
+    }
+
+    public HashMap<Integer, TitanRegistry> getTitansArchive() {
+        return titansArchive;
+    }
+
+    public ArrayList<Titan> getApproachingTitans() {
+        return approachingTitans;
+    }
+
+    public PriorityQueue<Lane> getLanes() {
+        return lanes;
+    }
+
+    public ArrayList<Lane> getOriginalLanes() {
+        return originalLanes;
+    }
 }
