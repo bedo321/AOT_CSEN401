301bc8718812d31b789e435c0d5b40203d047a3e
Bedo321 <abdalrhman.ashry@gmail.com>
2024-05-11 01:09:06 +0300

    Working for JavaFX

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module AOT.CSEN401 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swt;
+    requires junit;
+    exports game.tests;
+    exports game.gui;
+}
