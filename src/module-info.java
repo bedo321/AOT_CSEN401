2649000da6f6b05a130f6ccd0568236f77d715d3
Bedo321 <abdalrhman.ashry@gmail.com>
2024-05-15 04:06:41 +0300

    I firetrucked up and made it work again, I think

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module AOT.CSEN401 {
     requires javafx.fxml;
     requires javafx.base;
     requires javafx.graphics;
+    requires javafx.media;
     requires junit;
     exports game.tests;
     exports game.gui;
